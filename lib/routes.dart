28ed8b56cd13cb1a498cc976796a0db9cc4b7ec0
adnan morshed <adnan.starit118@gmail.com>
2022-08-02 18:17:28 +0600

    first commit

---
+++ lib/routes.dart
@@ -0,0 +1,16 @@
+
+import 'package:flutter/material.dart';
+
+class RoutesPage extends StatefulWidget {
+  const RoutesPage({Key? key}) : super(key: key);
+
+  @override
+  State<RoutesPage> createState() => _RoutesPageState();
+}
+
+class _RoutesPageState extends State<RoutesPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
